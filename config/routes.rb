840a1c88f633480860f7308ac470792f96b4c8d8
Guuri11 <sergio.gurillo11@gmail.com>
2021-08-04 17:48:08 +0200

    created ddbb schema & crud app

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
+  # resources manages to create all crud routes
+  resources :todos
+  resources :todo
   root 'home#index'
-  get 'home/about'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
